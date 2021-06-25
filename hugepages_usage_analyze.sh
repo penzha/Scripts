8a1ff2ec9485cd8103d911b62057330152a0643f
Pengfei Zhang <pezhang@zhangpegfeisMBP.sh.memverge.com>
2021-06-25 14:59:54 +0800

    add a wrapper for hugepages usage analyze

---
+++ hugepages_usage_analyze.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# show hugepages usage via matplotlib and install it if necessary
+# Author: pengfeiz
+
+python3 -m pip install --upgrade pip
+
+python3 -m pip install matplot
+
+python3 hugepages_usage.py
+
